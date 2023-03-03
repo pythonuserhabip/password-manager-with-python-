143456e1f96553bd993cbdb9aa36a1d2bba090be
pythonuserhabip <123060797+pythonuserhabip@users.noreply.github.com>
2023-03-03 12:05:13 +0300

    Update main.py

--- 29.day/main.py
+++ 29.day/main.py
@@ -107,7 +107,7 @@ website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "habip388@gmail.com")
+email_entry.insert(0, "YOUR EMAİL ADDRESS")
 password_entry = Entry(width=17)
 password_entry.grid(row=3, column=1)
 
